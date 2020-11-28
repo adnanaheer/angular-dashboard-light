a01557c101a78af04dad6329e626ee731477a282
adnanaheer <adnanaheer20@gmail.com>
2020-11-28 19:30:43 +0500

    updated dashboard cards layout

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -20,9 +20,9 @@ export class HomeComponent implements OnInit {
       }
 
       return [
-        { title: 'Card 1', cols: 2, rows: 1 },
+        { title: 'Card 1', cols: 1, rows: 1 },
         { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
+        { title: 'Card 3', cols: 1, rows: 1 },
         { title: 'Card 4', cols: 1, rows: 1 },
       ];
     })
@@ -37,15 +37,21 @@ export class HomeComponent implements OnInit {
           { title: 'Card 7', cols: 1, rows: 1 },
           { title: 'Card 8', cols: 1, rows: 1 },
           { title: 'Card 9', cols: 1, rows: 1 },
+          { title: 'Card 10', cols: 1, rows: 1 },
+          { title: 'Card 11', cols: 1, rows: 1 },
+          { title: 'Card 12', cols: 1, rows: 1 },
         ];
       }
 
       return [
-        { title: 'Card 5', cols: 2, rows: 1 },
+        { title: 'Card 5', cols: 1, rows: 1 },
         { title: 'Card 6', cols: 1, rows: 1 },
-        { title: 'Card 7', cols: 1, rows: 2 },
+        { title: 'Card 7', cols: 1, rows: 1 },
         { title: 'Card 8', cols: 1, rows: 1 },
         { title: 'Card 9', cols: 1, rows: 1 },
+        { title: 'Card 10', cols: 1, rows: 1 },
+        { title: 'Card 11', cols: 1, rows: 1 },
+        { title: 'Card 12', cols: 1, rows: 1 },
       ];
     })
   );
