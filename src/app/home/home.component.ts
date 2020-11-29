12b86da14f85bbbb06e8e53e1f3a0d9874dbd3d4
adnanaheer <adnanaheer20@gmail.com>
2020-11-29 21:47:20 +0500

    dashboard layout added cards

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,7 +8,7 @@ import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+  card1 = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({ matches }) => {
       if (matches) {
         return [
@@ -28,30 +28,78 @@ export class HomeComponent implements OnInit {
     })
   );
 
-  card = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+  card2 = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({ matches }) => {
       if (matches) {
         return [
-          { title: 'Card 5', cols: 1, rows: 1 },
+          { title: 'Card 5', cols: 2, rows: 1 },
           { title: 'Card 6', cols: 1, rows: 1 },
+        ];
+      }
+
+      return [
+        { title: 'Card 5', cols: 2, rows: 1 },
+        { title: 'Card 6', cols: 1, rows: 1 },
+      ];
+    })
+  );
+
+  card3 = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map(({ matches }) => {
+      if (matches) {
+        return [
           { title: 'Card 7', cols: 1, rows: 1 },
           { title: 'Card 8', cols: 1, rows: 1 },
           { title: 'Card 9', cols: 1, rows: 1 },
-          { title: 'Card 10', cols: 1, rows: 1 },
+        ];
+      }
+
+      return [
+        { title: 'Card 7', cols: 1, rows: 1 },
+        { title: 'Card 8', cols: 1, rows: 1 },
+        { title: 'Card 9', cols: 1, rows: 1 },
+      ];
+    })
+  );
+
+  card4 = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map(({ matches }) => {
+      if (matches) {
+        return [{ title: 'Card 10', cols: 2, rows: 2 }];
+      }
+
+      return [{ title: 'Card 10', cols: 2, rows: 2 }];
+    })
+  );
+
+  card5 = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map(({ matches }) => {
+      if (matches) {
+        return [
           { title: 'Card 11', cols: 1, rows: 1 },
           { title: 'Card 12', cols: 1, rows: 1 },
         ];
       }
 
       return [
-        { title: 'Card 5', cols: 1, rows: 1 },
-        { title: 'Card 6', cols: 1, rows: 1 },
-        { title: 'Card 7', cols: 1, rows: 1 },
-        { title: 'Card 8', cols: 1, rows: 1 },
-        { title: 'Card 9', cols: 1, rows: 1 },
-        { title: 'Card 10', cols: 1, rows: 1 },
         { title: 'Card 11', cols: 1, rows: 1 },
         { title: 'Card 12', cols: 1, rows: 1 },
+      ];
+    })
+  );
+
+  card6 = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map(({ matches }) => {
+      if (matches) {
+        return [
+          { title: 'Card 13', cols: 1, rows: 1 },
+          { title: 'Card 14', cols: 1, rows: 1 },
+        ];
+      }
+
+      return [
+        { title: 'Card 13', cols: 1, rows: 1 },
+        { title: 'Card 14', cols: 1, rows: 1 },
       ];
     })
   );
